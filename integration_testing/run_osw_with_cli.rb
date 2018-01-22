569a5f277fe35f35c62c7066e009feafba29b91d
Jason Turner <jason@emptycrate.com>
2018-01-22 10:59:30 -0700

    Make parallel module optional

--- integration_testing/run_osw_with_cli.rb
+++ integration_testing/run_osw_with_cli.rb
@@ -19,11 +19,19 @@ results_directories.each do |directory|
 
 end
 
-# run the jobs
-# if gem parallel isn't isntalled then comment out this coudl and use system(string) to run one job at a time
-require 'parallel'
-num_parallel = 6
-Parallel.each(jobs, in_threads: num_parallel) do |job|
-  puts job
-  system(job)
+begin
+  # run the jobs
+  # if gem parallel isn't installed then comment out this coudl and use system(string) to run one job at a time
+  require 'parallel'
+  num_parallel = 6
+  Parallel.each(jobs, in_threads: num_parallel) do |job|
+    puts job
+    system(job)
+  end
+rescue LoadError
+  puts "Unable to load 'parallel' module, running sequentially"
+  jobs.each do |job|
+    puts job
+    system(job)
+  end
 end
